d314d5d52e1cbad0ee5f50e229949c3aa0f7eed5
github username <github email address>
2019-07-08 14:22:58 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,63 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  
+  constructor() {
+  	super()
+
+  	this.state = {
+		sushis: [],
+    firstSushi: 0,
+    eatenIds: [],
+    budget: 100
+  	}
+  }
+
+  componentDidMount = () => {
+  	fetch(API)
+  	.then(resp => resp.json())
+    .then(sushis => {
+      sushis.map(sushi => sushi.eaten = false)
+  	  this.setState({sushis: sushis})
+    })
+  }
+
+  handleEat = sushi => {
+    if (sushi.eaten === false && this.state.budget >= sushi.price) {
+      this.setState(prevState => {
+        const sushis = prevState.sushis.slice();
+        const eatenIds = prevState.eatenIds.slice();
+
+        sushis.forEach(sus => {
+          if(sushi.id === sus.id) {sus.eaten = true}
+        })
+
+        eatenIds.push(sushi.id)
+
+        return {
+          sushis: sushis,
+          eatenIds: eatenIds,
+          budget: prevState.budget - sushi.price
+        };
+      });
+    }
+  }
+
+  handleClick = ev => {
+    this.setState(prevState => {
+      const beltPos = (prevState.firstSushi + 4) % prevState.sushis.length
+      return {firstSushi: beltPos }
+    })
+  }
+
 
   render() {
+    const itemNum = this.state.firstSushi
+    const filteredList = this.state.sushis.slice(itemNum, itemNum + 4 )
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushis={filteredList} handleEat={this.handleEat} handleClick={this.handleClick}/>
+        <Table sushiIds={this.state.eatenIds} budget={this.state.budget} />
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,17 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
-const Sushi = (props) => {
+const Sushi = ({sushi, handleEat}) => {
+  const {name, img_url, price, eaten} = sushi
+
+  const handleClick = () => handleEat(sushi)
   return (
-    <div className="sushi">
+    <div className="sushi" >
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+           onClick={handleClick}>
+        { eaten ? null : <img src={img_url} width="100%" alt="Yummy Sushi" /> }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   )
